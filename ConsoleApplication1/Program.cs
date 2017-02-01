e6d1790c38595328483faf5f5a7cfcad3eb0d14e
201555532 <201555532@P106-19>
2017-02-01 08:53:53 -0500

    Skrrrrrrrrrrt

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             // Les beaux malaises
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Fardouche!");
         }
     }
 }
